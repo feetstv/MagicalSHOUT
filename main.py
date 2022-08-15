39b0b85a1ce0377a7f392773763140891eb77916
Feets <747351+feetstv@users.noreply.github.com>
2022-08-15 13:24:37 +1200

    main: Remove QtQuick code

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-import sys
-from pathlib import Path
-
 from ShoutWindow import ShoutWindow
 from ShoutController import ShoutController
 from PySide6.QtWidgets import QApplication
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
 
-# if __name__ == "__main__":
-#     app = QGuiApplication(sys.argv)
-#     engine = QQmlApplicationEngine()
-#     qml_file = Path(__file__).resolve().parent / "ShoutQMLWindow.qml"
-#     engine.load(qml_file)
-#     if not engine.rootObjects():
-#         sys.exit(-1)
-#     app.exec()
 
 if __name__ == "__main__":
     app = QApplication()
